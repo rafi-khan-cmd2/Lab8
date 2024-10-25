ddcc000fe294a2f6b8171a462b8f78e6266a9cef
rafi-khan-cmd <rafiulal@ualberta.ca>
2024-10-25 14:20:26 -0600

    test case is failing for addCity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,13 +4,38 @@ package com.example.lab8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
+
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList mockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    private City mockCity(){
+        return new City("Edmonton","Alberta");
+    }
 
 
+    /**
+     * get the size of the list
+     * increase the list by adding a new citu
+     * check if our current size matches the initial size plus one
+     */
+    @Test
+    public void addCityTest() {
+        list = mockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan","SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+    
 }
